e155b3c0cbc159e1d8fa1b49597d86beb77fc85a
Nikola Heliskova <he.ni@seznam.cz>
2024-01-19 11:01:42 +0100

    Lekce01 - ukol - oprava casti kodu - zmena ukol5()

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,10 @@ public class Main {
     }
 
     public static void ukol5() {
-        double cena = 0; // Nastavíme do proměnné hodnotu 0
+        BigDecimal cena = BigDecimal.valueOf(0); // Nastavíme do proměnné hodnotu 0
         // Desetkrát přičteme hodnotu jedna desetina (0,1):
         for (int i = 0; i < 10; i++) {
-            cena += 0.1;
+            cena = cena.add(BigDecimal.valueOf(0.1));
                // operátor "+=" znamená: přičti ke stávající
                // hodnotě navíc 0.1
         }
